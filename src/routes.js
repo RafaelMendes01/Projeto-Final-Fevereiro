954ea09138b249c7dfd4c09a12d08e4558be9840
Rafael <rafael.mendes@tallos.com.br>
2022-03-02 15:44:37 -0300

    get adicionado a routes e conexao com server

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
+routes.get('/', (req , res) =>{
+    return res.status(200).json("rota em funcionamento")
+})
+
 module.exports = routes;
