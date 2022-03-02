091cf0fa1fea23442b6005175c92e44b21fa2c06
Rafael <rafael.mendes@tallos.com.br>
2022-03-02 17:11:20 -0300

    conexao criada no db usando mongoose

---
+++ src/db/conexao.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const conexao = {
+    isConnected:0
+};
+
+const uri = process.env.MONGO_URI;
+
+const connect = async () =>{
+    if(conexao.isConnected){
+        return;
+    }
+    const db = await mongoose.connect(uri);
+    conexao.isConnected = db.connections[0].readyState;
+};
+module.exports = connect;
