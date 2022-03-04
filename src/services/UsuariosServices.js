07e917e16a337cf958a89e610d7e434cbe791b6a
rafael mendes <rafael.mendes@tallos.com.br>
2022-03-04 09:44:49 -0300

    bugs de bcypt resolvidos e senha hash criada

--- src/services/UsuariosServices.js
+++ src/services/UsuariosServices.js
@@ -1,3 +1,4 @@
+const { hash } = require('bcrypt')
 const connect = require('../db/conexao');
 const UsuariosSchema = require('../db/schemas/UsuariosSchemas')
 class UsuariosServices{
@@ -9,10 +10,11 @@ class UsuariosServices{
         const {nome,email,senha} = req.body;
         try {
             await this.conexao();
+            const hashSenha = await hash(senha, 11)
             const usuarios = await this.usuarios.create({
                 nome,
                 email,
-                senha
+                senha: hashSenha
             })
             const {senha: SenhaDoUsuario, ... resto} = usuarios._doc;
             return res.status(201).json({usuarios:resto});  
