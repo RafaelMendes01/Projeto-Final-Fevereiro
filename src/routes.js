866a6ce35a8768a43cb7aa9c132acdf584e61a10
rafael mendes <rafael.mendes@tallos.com.br>
2022-03-07 11:56:30 -0300

    middleware inserido

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const UsuariosControllers = require('./controllers/UsuariosControllers');
 const AuthController = require('./controllers/auth_controller');
+const middleware = require("./middleware/validarToken");
 const UsuariosSchema = require('./db/schemas/UsuariosSchemas');
 const usuarioscontroller = new UsuariosControllers();
 const authController = new AuthController;
@@ -10,15 +11,20 @@ const routes = express.Router();
 routes.get('/', (req , res) =>{
     return res.status(200).json("rota em funcionamento")
 })
+routes.route('/login')
+ .post(async (req,res) => await authController.loginController(req,res))
+
 routes.route('/usuarios')
 .post((req,res)=>{
     return usuarioscontroller.createControllers(req,res)
 })
+
+routes.use(new middleware().validateToken)
+
+routes.route('/usuarios')
 .get(async (req,res) => await usuarioscontroller.getAllController(req,res))
 
 routes.route('/usuarios/show')
 .get(async (req,res) => usuarioscontroller.showController(req,res))
 
-routes.route('/login')
- .post(async (req,res) => await authController.loginController(req,res))
 module.exports = routes;
